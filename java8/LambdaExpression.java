f9e47ed5a82f4aed8839fbb1b554ebc6780adc6c
Ramesh Katakam <katakamramesh52@gmail.com>
2025-07-08 21:44:51 +0530

    java 8 lambda expression

---
+++ java8/LambdaExpression.java
@@ -0,0 +1,25 @@
+Class FunctionProgramming{
+
+    //what is lambda expresion --> it is a anonmys fuction which has no return , no access modifier, no method name
+
+    //before java 8 - no args
+    public void print(){
+        System.out.println("all is ok");
+    }
+    //in java 8 () -> sout("all is ok);
+
+    //before java 8 -- with args
+    public void sum(int a , int b){
+        System.out.println(a + b);
+    }
+    //in java 8 (a,b) -> sout(a + b);
+
+    //before java 8 -- with return and args
+    public int sum(int a{
+        return a * a;
+    }
+    //in java 8 (a) -> a * a or a -> a * a;
+    //if we want retunr statment then we need to mention {} like a -> {return (a * a);}
+
+    //semi colon is required in case of {} in lambda expersion
+}
