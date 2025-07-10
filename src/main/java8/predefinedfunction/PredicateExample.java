a65aa07af2a5eb1b0b036139ae09c45321d46c1c
Ramesh Katakam <katakamramesh52@gmail.com>
2025-07-11 00:03:29 +0530

    Another Predicate Example

--- src/main/java8/predefinedfunction/PredicateExample.java
+++ src/main/java8/predefinedfunction/PredicateExample.java
@@ -1,5 +1,6 @@
 package main.java8.predefinedfunction;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -20,5 +21,39 @@ public class PredicateExample {
         }
 
 
+        // Ex4: Single employee test
+        Employee emp = new Employee("John", 20000, 2);
+
+        // Predicate: Salary > 30000 and Experience > 3
+        Predicate<Employee> pr = e -> e.salary > 30000 && e.experience > 3;
+        System.out.println(pr.test(emp));  // Output: false
+
+        // Ex5: Filter a list of employees
+        ArrayList<Employee> al = new ArrayList<>();
+        al.add(new Employee("John", 50000, 5));
+        al.add(new Employee("David", 20000, 2));
+        al.add(new Employee("Scott", 30000, 3));
+        al.add(new Employee("Scott", 40000, 6));
+
+        for (Employee e : al) {
+            if (pr.test(e)) {
+                System.out.println(e.ename + " " + e.salary);
+            }
+        }
+
+    }
+
+
+}
+
+class Employee {
+    String ename;
+    double salary;
+    int experience;
+
+    Employee(String ename, double salary, int experience) {
+        this.ename = ename;
+        this.salary = salary;
+        this.experience = experience;
     }
 }
